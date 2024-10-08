c64795bd2328ef542ecf35617f662dbf35dabf0b
Boocheiei <boochveerapat@gmail.com>
2024-10-08 11:55:45 +0700

    setup

---
+++ src/components/main/main.js
@@ -0,0 +1,17 @@
+import './main.css';
+
+function Main(){
+    return(
+        <div className="grid-main">
+            <div className='grid-main-item1'>
+               <div>main</div>
+            </div>
+            <h2>hello</h2>
+            <h2>hello</h2>
+            <h2>hello</h2>
+            <h2>hello</h2>
+            <h2>hello</h2>
+        </div>
+    );
+}
+export default Main;
---
+++ src/components/nav/nav.js
@@ -0,0 +1,18 @@
+import './nav.css';
+
+function Nav (){
+    return (
+    <div className="grid-nav">
+        <div>
+            <h1>hello</h1>
+        </div>
+        <div>
+            <h1>hello</h1>
+        </div>
+        <div>
+            <h1>hello</h1>
+        </div>
+    </div>
+    );
+}
+export default Nav;
